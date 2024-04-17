c07ead3e746d90b80e321233989448a9513cff70
Mario Zito <mario.zito@treme.io>
2024-04-17 15:48:28 -0300

    Update schema and columns names for credentials

--- prisma/update-patches.sql
+++ prisma/update-patches.sql
@@ -3,3 +3,7 @@
 ALTER TABLE IF EXISTS public.members RENAME "communityUid" TO community_uid;
 ALTER TABLE IF EXISTS public.members RENAME "personUid" TO person_uid;
 ALTER TABLE IF EXISTS public.members RENAME "approvedUTC" TO approved_utc;        
+
+ALTER TABLE IF EXISTS public.credentials RENAME "communityUid" TO community_uid;        
+ALTER TABLE IF EXISTS public.credentials RENAME "applicantUid" TO applicant_uid;        
+ALTER TABLE IF EXISTS public.credentials RENAME "claimUid" TO claim_uid;        
