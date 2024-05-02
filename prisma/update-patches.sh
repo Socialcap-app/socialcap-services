a8ca5d6203bd2d53cc34f39265abc36e07843105
Mario Zito <mario.zito@treme.io>
2024-05-02 16:02:26 -0300

    Fix schema scripts & updates

--- prisma/update-patches.sh
+++ prisma/update-patches.sh
@@ -5,7 +5,9 @@
 # or:
 #       sh prisma/update-views.sh dev # updates socialcapdev views
 #
+sudo -u postgres psql -d socialcap$1 < prisma/fix-columns.sql
 sudo -u postgres psql -d socialcap$1 < prisma/claims-view.sql
 sudo -u postgres psql -d socialcap$1 < prisma/tasks-view.sql
 sudo -u postgres psql -d socialcap$1 < prisma/transactions-view.sql
 sudo -u postgres psql -d socialcap$1 < prisma/plans-view.sql
+
