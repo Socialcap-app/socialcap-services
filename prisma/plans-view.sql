18fb18c8f45f2908e115189c90adf5086a0ab2ec
Mario Zito <mario.zito@treme.io>
2024-05-02 10:59:20 -0300

    Fix pland_view include all cols

--- prisma/plans-view.sql
+++ prisma/plans-view.sql
@@ -1,30 +1,14 @@
--- View: public.plans_view
-
 DROP VIEW public.plans_view;
 
 CREATE OR REPLACE VIEW public.plans_view
  AS
- SELECT pl.uid,
-    pl.name,
-    pl.description,
-    pl.image,
+ SELECT pl.*,
     '' AS banner,
-    pl.alias,
-    pl.state,
     st.label AS state_descr,
-    pl.created_utc,
-    pl.updated_utc,
-    pl.approved_utc,
-    pl.starts_utc,
-    pl.ends_utc,
-    pl.available,
-    pl.total,
-    pl.fee,
     cm.name AS community,
-    cm.uid AS community_uid,
-	cm.image AS community_image,
+    cm.image AS community_image,
     concat(cm.xadmins, ',', cm.admin_uid) AS admins
-   FROM communities cm,
+  FROM communities cm,
     plans pl,
     states st
   WHERE pl.state = st.id AND cm.uid = pl.community_uid;
